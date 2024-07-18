044eb2e36e1c14a9e62b98c3eeba7993cf6e89e2
Rickardo-769 <167241461+Rickardo-769@users.noreply.github.com>
2024-07-19 00:00:00 +0500

    'add file game.py'

---
+++ game.py
@@ -0,0 +1,22 @@
+# game.py
+import random
+from player import Player
+from number import Number
+from feedback import Feedback
+from config import LOWER_BOUND, UPPER_BOUND
+
+class Game:
+    def __init__(self) -> None:
+        self.player = Player()
+        self.number = Number()
+        self.feedback = Feedback()
+
+    def start(self) -> None:
+        print(f"I'm thinking of a number between {LOWER_BOUND} and {UPPER_BOUND}.")
+        while True:
+            guess = self.player.make_guess()
+            if self.number.check_guess(guess):
+                print(self.feedback.congratulations())
+                break
+            else:
+                print(self.feedback.hint(guess))
